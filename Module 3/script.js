8c0b69c1319a5a958b44289036cc5244df64c487
corygyarmathy <69447325+corygyarmathy@users.noreply.github.com>
2023-03-13 09:09:46 +0000

    added script for game logic, TTS

--- Module 3/script.js
+++ Module 3/script.js
@@ -3,14 +3,22 @@ The Computer ALWAYS picks ROCK.
 Does the user's choose win,lose, or tie vs ROCK? */
 function play(user) {
     let result = "";
-    if (user === 'rock') {
+    if (user === "rock") {
         result = "tie";
     }
-    if (user === 'paper') {
+    if (user === "paper") {
         result = "win";
     }
-    if (user === 'scissors') {
+    if (user === "scissors") {
         result = "lose";
     }
-    alert("Computer chose rock, you " + result + "!");
+    var theMessage = ("Computer chose rock, you " + result + "!");
+    alert(theMessage);
+    say(theMessage);
+}
+
+function say(myMessage) {
+    const synth = window.speechSynthesis;
+    const utterThis = new SpeechSynthesisUtterance(myMessage);
+    synth.speak(utterThis)
 }
